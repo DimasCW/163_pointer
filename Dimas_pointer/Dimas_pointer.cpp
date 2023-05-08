0cb4a36ebda211513b11eafc5971ed2b46ae56dd
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-08 17:23:15 +0700

    membuat dan mengisi int main

--- Dimas_pointer/Dimas_pointer.cpp
+++ Dimas_pointer/Dimas_pointer.cpp
@@ -11,3 +11,17 @@ void mahasiswa::showNim() {		//implementasi methode di luar class
 	cout << "No Induk = " << nim << endl;
 }
 
+int main()
+{
+	mahasiswa mhs{ 1 };		//objek mhs
+	mhs.showNim();			//Member Access Operator
+
+	mahasiswa& ref = mhs;		//pointer References refMhs
+	ref.nim = 2;				//Member Access Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;		//Pointer  Dereferences pMhs
+	pMhs->nim = 3;				//Arrow Operator
+	mhs.showNim();
+	return 0;
+}
